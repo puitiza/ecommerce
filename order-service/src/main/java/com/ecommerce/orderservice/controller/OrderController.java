f6166aaf67379014b0b5b513ce910652088d6d18
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2023-11-27 12:38:55 -0500

    Distributed Tracing by Zipkin,micrometer (api-gateway, order-service, product-service)

--- order-service/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.ecommerce.orderservice.controller;
 import com.ecommerce.orderservice.model.Order;
 import com.ecommerce.orderservice.model.OrderWithProducts;
 import com.ecommerce.orderservice.services.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +15,24 @@ import java.util.List;
 @RequestMapping("/order")
 @RestController
 public record OrderController(OrderService orderService) {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     @GetMapping("/{orderId}")
     public Order getOrderById(@PathVariable Long orderId) {
+        logger.info("GETTING ORDER WITH ID {}", orderId);
         return orderService.getOrderById(orderId);
     }
 
     @GetMapping("/all")
     public List<Order> getAllOrders() {
+        logger.info("GETTING ALL ORDERS");
         return orderService.getAllOrders();
     }
 
     @GetMapping("/withProducts/{orderId}")
     public OrderWithProducts getOrderWithProducts(@PathVariable Long orderId) {
+        logger.info("COLLECTING ORDER AND PRODUCT WITH ID {} FROM UPSTREAM SERVICE", orderId);
         return orderService.getOrderWithProducts(orderId);
     }
 }
--- product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.ecommerce.productservice.controller;
 
 import com.ecommerce.productservice.model.Product;
 import com.ecommerce.productservice.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,18 +13,22 @@ import java.util.List;
 @RestController
 public record ProductController(ProductService productService) {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
     @GetMapping("/{productId}")
     public Product getProductById(@PathVariable Long productId) {
+        logger.info("GETTING PRODUCT WITH ID {}", productId);
         return productService.getProductById(productId);
     }
 
     @GetMapping("/all")
     public List<Product> getAllProducts() {
+        logger.info("GETTING ALL PRODUCTS");
         return productService.getAllProducts();
     }
 
     @GetMapping("/byOrderId")
     public List<Product> getProductsByOrderId(@RequestParam Long orderId) {
+        logger.info("COLLECTING LIST PRODUCT BY ORDER_ID {} FROM UPSTREAM SERVICE", orderId);
         return productService.getProductsByOrderId(orderId);
     }
 }
