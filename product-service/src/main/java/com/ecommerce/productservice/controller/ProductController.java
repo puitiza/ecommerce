140be205262bdb21f16b25ba9982d6093232dbf5
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2023-12-14 17:38:08 -0500

    clean import

--- product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -9,8 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
