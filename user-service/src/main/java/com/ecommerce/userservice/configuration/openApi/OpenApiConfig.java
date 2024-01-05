d31c8485cd502b14915835fd730c20ce910ba037
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2024-01-05 17:35:45 -0500

    clean imports

--- user-service/src/main/java/com/ecommerce/userservice/configuration/openApi/OpenApiConfig.java
+++ user-service/src/main/java/com/ecommerce/userservice/configuration/openApi/OpenApiConfig.java
@@ -1,12 +1,9 @@
 package com.ecommerce.userservice.configuration.openApi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.*;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
