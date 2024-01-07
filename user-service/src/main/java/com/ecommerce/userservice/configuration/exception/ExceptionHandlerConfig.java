d639bde57f8936c221e2a47b19b511c777edb90f
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2024-01-07 16:29:59 -0500

    optimize imports

--- user-service/src/main/java/com/ecommerce/userservice/configuration/exception/ExceptionHandlerConfig.java
+++ user-service/src/main/java/com/ecommerce/userservice/configuration/exception/ExceptionHandlerConfig.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
