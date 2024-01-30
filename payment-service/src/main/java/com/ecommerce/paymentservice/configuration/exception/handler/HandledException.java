b8b4f2a32f385593c1a5b55fa07c2da002bcbee7
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2024-01-30 00:02:02 -0500

    clean unused

--- payment-service/src/main/java/com/ecommerce/paymentservice/configuration/exception/handler/HandledException.java
+++ payment-service/src/main/java/com/ecommerce/paymentservice/configuration/exception/handler/HandledException.java
@@ -2,10 +2,6 @@ package com.ecommerce.paymentservice.configuration.exception.handler;
 
 public abstract class HandledException extends RuntimeException {
 
-    protected HandledException(String message, Throwable e) {
-        super(message, e);
-    }
-
     protected HandledException(String message) {
         super(message);
     }
