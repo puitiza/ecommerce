9a839752fd1e338001e19f81ff5a7ca1fa01243d
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2025-07-08 20:21:41 -0500

    clean import statement

--- payment-service/src/main/java/com/ecommerce/paymentservice/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/ecommerce/paymentservice/repository/PaymentRepository.java
@@ -4,7 +4,6 @@ import com.ecommerce.paymentservice.model.entity.PaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
