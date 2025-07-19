898ef1a6221ae1cca60ed039d3f4cbbd137d31b8
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2025-07-18 20:56:47 -0500

    clean import

--- shared-library/src/main/java/com/ecommerce/shared/exception/ErrorResponse.java
+++ shared-library/src/main/java/com/ecommerce/shared/exception/ErrorResponse.java
@@ -1,9 +1,8 @@
 package com.ecommerce.shared.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Objects;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
