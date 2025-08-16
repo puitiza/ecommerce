884da60943965de7fb2e6525dba3fa0018105647
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2025-08-16 17:56:38 -0500

    fix Product Fallback methods

--- order-service/src/main/java/com/ecommerce/orderservice/infrastructure/adapter/feign/ProductFeignAdapter.java
+++ order-service/src/main/java/com/ecommerce/orderservice/infrastructure/adapter/feign/ProductFeignAdapter.java
@@ -1,16 +1,17 @@
 package com.ecommerce.orderservice.infrastructure.adapter.feign;
 
-import com.ecommerce.orderservice.application.dto.BatchProductDetailsRequest;
-import com.ecommerce.orderservice.application.dto.BatchProductDetailsResponse;
-import com.ecommerce.orderservice.application.dto.BatchProductRequest;
-import com.ecommerce.orderservice.application.dto.BatchProductResponse;
+import com.ecommerce.orderservice.application.dto.*;
 import com.ecommerce.orderservice.application.port.out.ProductServicePort;
+import com.ecommerce.orderservice.domain.exception.OrderValidationException;
+import com.ecommerce.shared.exception.ExceptionError;
+import feign.FeignException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -21,42 +22,47 @@ public class ProductFeignAdapter implements ProductServicePort {
     private final ProductFeignClient productFeignClient;
 
     @Override
-    @CircuitBreaker(name = "productServiceCircuit")
+    @CircuitBreaker(name = "productServiceCircuit", fallbackMethod = "verifyAndGetProductsFallback")
     public BatchProductResponse verifyAndGetProducts(BatchProductRequest items, String token) {
         return productFeignClient.verifyAndGetProducts(items, token);
     }
 
     @Override
-    @CircuitBreaker(name = "productServiceCircuit")
+    @CircuitBreaker(name = "productServiceCircuit", fallbackMethod = "getProductsDetailsInBatchFallback")
     public List<BatchProductDetailsResponse> getProductsDetailsInBatch(BatchProductDetailsRequest request, String token) {
         return productFeignClient.getProductsDetailsInBatch(request, token);
     }
 
-    /*@SuppressWarnings("unused")
-    private ProductResponse getProductFallback(Long id, String token, Throwable t) {
-        throw handleError(1L, t, "Failed to retrieve product");
+    @SuppressWarnings("unused")
+    private BatchProductResponse verifyAndGetProductsFallback(BatchProductRequest items, String token, Throwable t) {
+        List<Long> productIds = items.items().stream().map(OrderItemRequest::productId).toList();
+        String errorMessage = "Failed to verify and retrieve products in batch";
+        throw handleError(errorMessage, productIds, t);
     }
 
     @SuppressWarnings("unused")
-    private void updateInventoryFallback(Long id, int updatedInventory, String token, Throwable t) {
-        throw handleError(2L, t, "Failed to update product inventory");
+    private List<BatchProductDetailsResponse> getProductsDetailsInBatchFallback(BatchProductDetailsRequest request, String token, Throwable t) {
+        String errorMessage = "Failed to retrieve product details in batch";
+        throw handleError(errorMessage, request.productIds(), t);
     }
 
-    private RuntimeException handleError(Long id, Throwable t, String msg) {
-        log.error("{} for ID {}: {}", msg, id, t.getMessage(), t);
-        String details = t.getMessage() != null ? t.getMessage() : msg + " for ID " + id;
+    private OrderValidationException handleError(String errorMessage, List<Long> productIds, Throwable t) {
+        String details = t.getMessage() != null ? t.getMessage() : errorMessage + " for products: " + productIds;
+        String productIdsStr = productIds.stream().map(String::valueOf).collect(Collectors.joining(","));
+        log.error("{} for products {}: {}", errorMessage, productIds, t.getMessage(), t);
+
         if (t instanceof FeignException feign) {
-            return switch (feign.status()) {
-                case 404 -> new ResourceNotFoundException(SERVICE_NAME, id.toString());
-                case 429 -> new OrderValidationException(ExceptionError.GATEWAY_RATE_LIMIT,
-                        "Rate limit exceeded for ID " + id);
-                case 503, -1 -> new OrderValidationException(ExceptionError.SERVICE_UNAVAILABLE,
-                        details, SERVICE_NAME, id);
-                default -> new OrderValidationException(ExceptionError.ORDER_PRODUCT_AVAILABILITY_CHECK_FAILED,
-                        details, id);
-            };
+            switch (feign.status()) {
+                case 503, -1 -> {
+                    return new OrderValidationException(ExceptionError.SERVICE_UNAVAILABLE, details, SERVICE_NAME, productIdsStr);
+                }
+                default -> {
+                    return new OrderValidationException(ExceptionError.ORDER_PRODUCT_AVAILABILITY_CHECK_FAILED,
+                            details, productIdsStr
+                    );
+                }
+            }
         }
         return new OrderValidationException(ExceptionError.INTERNAL_SERVER_ERROR, details);
     }
-     */
 }
